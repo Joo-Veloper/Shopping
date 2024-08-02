af4a8df4b50d9e1e5bef167fbb1bdb1457d7c338
Seung Joo <psjoo3515@gmail.com>
2024-08-02 15:56:39 +0900

    Member Domain Test

---
+++ src/main/java/io/core/member/playtest/MemberApp.java
@@ -0,0 +1,19 @@
+package io.core.member.playtest;
+
+import io.core.member.entity.Grade;
+import io.core.member.entity.Member;
+import io.core.member.service.MemberService;
+import io.core.member.service.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+
+    }
+}
