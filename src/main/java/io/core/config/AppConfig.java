b268fdcc6cf484572def195a556fb72813930cad
Seung Joo <psjoo3515@gmail.com>
2024-08-04 16:38:14 +0900

    AppConfig-Refactoring

--- src/main/java/io/core/config/AppConfig.java
+++ src/main/java/io/core/config/AppConfig.java
@@ -1,6 +1,8 @@
 package io.core.config;
 
+import io.core.discount.repository.DiscountPolicy;
 import io.core.discount.repository.FixDiscountPolicy;
+import io.core.member.repository.MemberRepository;
 import io.core.member.repository.MemoryMemberRepository;
 import io.core.member.service.MemberService;
 import io.core.member.service.MemberServiceImpl;
@@ -9,11 +11,19 @@ import io.core.order.service.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
