879c725b80f2e5aa5379120ba3ca8b5322b83725
Seung Joo <psjoo3515@gmail.com>
2024-08-06 14:46:26 +0900

    Singleton & Configuration

---
+++ src/test/java/io/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,28 @@
+package io.core.singleton;
+
+import io.core.config.AppConfig;
+import io.core.member.repository.MemberRepository;
+import io.core.member.service.MemberServiceImpl;
+import io.core.order.service.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ConfigurationSingletonTest {
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        System.out.println("memberService -> memberRepository = " + memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+        //모두 같은 인스턴스를 참고하고 있다.
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
