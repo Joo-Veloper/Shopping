e9a8ba500d3234117ad8f314c32e0b1746b7b942
Seung Joo <psjoo3515@gmail.com>
2024-08-07 20:55:19 +0900

    Error 처리

--- src/test/java/io/core/SpringCoreApplicationTests.java
+++ src/test/java/io/core/SpringCoreApplicationTests.java
@@ -1,9 +1,10 @@
 package io.core;
 
+import io.core.global.AutoAppConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AutoAppConfig.class)
 class SpringCoreApplicationTests {
 
     @Test
