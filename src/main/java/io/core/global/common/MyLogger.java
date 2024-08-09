f2324c84ca2a8fcf0bb264aaf153a281524c8e66
Seung Joo <psjoo3515@gmail.com>
2024-08-09 18:25:44 +0900

    Scope and Proxy

--- src/main/java/io/core/global/common/MyLogger.java
+++ src/main/java/io/core/global/common/MyLogger.java
@@ -3,15 +3,21 @@ package io.core.global.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+/** ScopedProxyMode.TARGET_CLASS
+ * 적용 대상이 인터페이스가 아닌 클래스 : TARGET_CLASS
+ * 적용 대상이 인터페이스면 : INTERFACE를 선택
+ */
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
     private String uuid;
     private String requestURL;
+
     public void setRequestURL(String requestURL) {
         this.requestURL = requestURL;
     }
@@ -19,6 +25,7 @@ public class MyLogger {
     public void log(String message) {
         System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
     }
+
     /**
      * 빈이 생성된 후 초기화 작업을 수행하는 메서드입니다.
      * 이 메서드는 스프링이 빈을 생성하고 의존성을 주입한 후에 자동으로 호출됩니다.
--- src/main/java/io/core/global/web/service/LogDemoService.java
+++ src/main/java/io/core/global/web/service/LogDemoService.java
@@ -2,15 +2,14 @@ package io.core.global.web.service;
 
 import io.core.global.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+
     public void logic(String id) {
-        myLoggerProvider.getObject();
-        myLoggerProvider.getObject().log("service id = " + id);
+        myLogger.log("service id = " + id);
     }
 }
